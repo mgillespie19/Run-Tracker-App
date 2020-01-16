daf6a7c02b65b1661e219037ee7668474fe9f106
Max Gilespie <max.gillespie10@gmail.com>
2020-01-16 16:18:56 -0600

    forgot to add the actual app lol.

---
+++ Run Tracker/Run Tracker/View/RootTabView.swift
@@ -0,0 +1,21 @@
+//
+//  RootTabView.swift
+//  Run Tracker
+//
+//  Created by Max Gillespie on 1/16/20.
+//  Copyright © 2020 Max Gillespie. All rights reserved.
+//
+
+import SwiftUI
+
+struct RootTabView: View {
+    var body: some View {
+        Text("Tab View")
+    }
+}
+
+struct RootTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootTabView()
+    }
+}
